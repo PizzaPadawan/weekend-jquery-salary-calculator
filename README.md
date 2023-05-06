d6b0b39c0d7d1e6dc9fad82c78d50b46dd02208c
Kord Maurer <kord.r.maurer@gmail.com>
2023-05-06 13:08:58 -0500

    updated deleteEmployee to decrement correct amount from totalMonthly

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Additional README details can be found [here](https://github.com/PrimeAcademy/re
 
 HTML:
 - [x] "Salary Calculator" header section
-- [x]"Add Employee" section with inputs for First Name, Last Name, ID, Title, and Annual Salary, along with a Submit button
+- [x] "Add Employee" section with inputs for First Name, Last Name, ID, Title, and Annual Salary, along with a Submit button
 - [x] "Employees" table with the above listed data as th columns in thead
 - [x] "Total Monthly" with span to keep track of total monthly cost
 
@@ -20,10 +20,10 @@ JS:
 - [x] Add click event to add input.vals to employee table on submitButton click, along with a delete button for each row
 - [x] Add alert to fire if user attempts to submit with any inputs left blank
 - [x] Clear inputs after submitButton click event
-- [] Activate delete button via descendent selector
-- [] Calculate monthly costs in annualSalary field and append the amount to Total Monthly span
-- [] Format Total Monthly span to turn red if amount is > 20,000
-- [] STRETCH: Update the Total Monthly span to decrement once an employee is deleted.
+- [x] Activate delete button via descendent selector
+- [x] Calculate monthly costs in annualSalary field and append the amount to Total Monthly span
+- [x] Format Total Monthly span to turn red if amount is > 20,000
+- [x] STRETCH: Update the Total Monthly span to decrement once an employee is deleted.
 
 CSS: 
 - [] STRETCH: Make it STYLIN. I'm thinking 90's office chic. [unsplash](https://unsplash.com/) for images. Maybe a Windows 98 theme?
--- scripts/client.js
+++ scripts/client.js
@@ -1,14 +1,17 @@
 $(function(){
    $("#submitButton").on('click', addEmployee) 
+   $("#employeeTable tbody").on('click', '.deleteButton', deleteEmployee)
 })
 
 //adding a global array to store salaries
 let employeeSalaries = []
 let monthlyCost = 0;
+let salaryId = 0;
 
 function addEmployee(){
 // we need to append input values to the employeeTable
 // but only if all fields are filled in
+    
 
     if($("#firstName").val() === '' ||
         $("#lastName").val() === '' ||
@@ -17,20 +20,22 @@ function addEmployee(){
         $("#annualSalary").val() === ''){
         alert("Please fill out all fields");
     } else {
-    
+        // adding annualSalary values to an array
+        // may be more useful for totalMonthly calculation
+        employeeSalaries.push(Math.round(Number($("#annualSalary").val() / 12)));
     // appending input values to the employeeTable
+    // STRETCH: Adding salary class to applicable td for decrement function
     $("#employeeTable tbody").append(`
     <tr>
         <td>${$("#firstName").val()}</td>
         <td>${$("#lastName").val()}</td>
         <td>${$("#employeeNumber").val()}</td>
         <td>${$("#employeeTitle").val()}</td>
-        <td>${$("#annualSalary").val()}</td>
-    <td><button id="deleteButton">Delete</button></td>
+        <td class="salary" data-salary="${salaryId}">${$("#annualSalary").val()}</td>
+    <td><button class="deleteButton">Delete</button></td>
     </tr>
     `)
-    employeeSalaries.push(Number($("#annualSalary").val() / 12));
-    monthlyCost += Math.round(Number($("#annualSalary").val() / 12));
+     monthlyCost += (Math.round(Number($("#annualSalary").val() / 12)))
     // emptying inputs
     $("#firstName").val('')
     $("#lastName").val('')
@@ -38,20 +43,35 @@ function addEmployee(){
     $("#employeeTitle").val('')
     $("#annualSalary").val('')
     }
-
-    calculateTotal();
+    salaryId ++
+    showTotal();
 }
 
-function calculateTotal(){
+function showTotal(){
     // we need to access each #annualSalary in the table,
     // divide it by 12, add it to a global variable,
     // and append it to the totalMonthly span.
-    // tried a for loop and that didn't work because duh
+
+    // ** tried a for loop and that didn't work because duh
     // gonna throw the dividing and adding functions into
     // addEmployee function
-    $("#totalMonthly").text(monthlyCost);
+    $("#totalMonthly").text(`$${monthlyCost}.00`);
 
     if(monthlyCost > 20000){
         $("#totalMonthly").css("color", "red");
     }
 }
+
+function deleteEmployee(event){
+    // for (let i = 0; i < employeeSalaries.length; i++){
+    //     if($(event.target).closest("td .salary").data("salary") === ){
+    //         monthlyCost -= employeeSalaries[i];
+    //     }
+    // }
+    monthlyCost -= (Number($(event.target).closest("tr").find("td.salary").text()) / 12)
+    salaryId --
+    showTotal();
+    $(event.target).closest("tr").remove();
+}
+
+
